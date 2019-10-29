97bdef2c8247f842524fa503937d269a27d00502
Ankur Jain <ajain3@expedia.com>
2019-10-29 23:02:07 +0530

    added get method in InvalidIPException

--- src/main/java/in/ankushs/dbip/exceptions/InvalidIPException.java
+++ src/main/java/in/ankushs/dbip/exceptions/InvalidIPException.java
@@ -1,8 +1,14 @@
 package in.ankushs.dbip.exceptions;
 
-public class InvalidIPException extends RuntimeException{
-	private String message;
-	public InvalidIPException(final String message){
-		super(message);
-	}
+public class InvalidIPException extends RuntimeException {
+    private String message;
+
+    public InvalidIPException(final String message) {
+        super(message);
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
 }
