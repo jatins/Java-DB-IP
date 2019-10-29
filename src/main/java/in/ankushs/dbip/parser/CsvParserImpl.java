70c75448d6186fb48f35c2b682a2b7b3a62e5752
Ankur Jain <ajain3@expedia.com>
2019-10-29 23:09:01 +0530

    refactored code; used Optional.ofNullable for better readability

--- src/main/java/in/ankushs/dbip/parser/CsvParserImpl.java
+++ src/main/java/in/ankushs/dbip/parser/CsvParserImpl.java
@@ -1,30 +1,28 @@
 package in.ankushs.dbip.parser;
 
 import java.util.Arrays;
+import java.util.Optional;
 
 import in.ankushs.dbip.utils.PreConditions;
 
 public final class CsvParserImpl implements CsvParser {
-	
-	public static CsvParserImpl parser = null;
-	
-	private CsvParserImpl(){}
-	public static CsvParserImpl getInstance(){
-		if(parser==null){
-			return new CsvParserImpl();
-		}
-		return parser;
-	}
 
-	@Override
-	public String[] parseRecord(final String csvRecord) {
-		PreConditions.checkEmptyString(csvRecord, "null or empty csvRecord was passed");
-		
-		return Arrays.stream(csvRecord.split(","))
-				.map(str ->{
-					return str.replace("\"", "").trim();
-				})
-				.toArray(String[]::new);
-	}
+    public static CsvParserImpl parser = null;
+
+    private CsvParserImpl() {
+    }
+
+    public static CsvParserImpl getInstance() {
+        return Optional.ofNullable(parser).orElse(new CsvParserImpl());
+    }
+
+    @Override
+    public String[] parseRecord(final String csvRecord) {
+        PreConditions.checkEmptyString(csvRecord, "null or empty csvRecord was passed");
+
+        return Arrays.stream(csvRecord.split(","))
+                .map(str -> str.replace("\"", "").trim())
+                .toArray(String[]::new);
+    }
 
 }
