797b907cb83e8b51c9c4b95c8650b66a451775db
Ankur Jain <ajain3@expedia.com>
2019-10-29 23:03:10 +0530

    removed unused variable

--- src/main/java/in/ankushs/dbip/exceptions/InvalidIPException.java
+++ src/main/java/in/ankushs/dbip/exceptions/InvalidIPException.java
@@ -1,14 +1,8 @@
 package in.ankushs.dbip.exceptions;
 
 public class InvalidIPException extends RuntimeException {
-    private String message;
 
     public InvalidIPException(final String message) {
         super(message);
-    }
-
-    @Override
-    public String getMessage() {
-        return message;
     }
 }
